4f3297626e29dfbbd9d344b4d3c2e400bd4e1b4f
Fidy <fidygalatyne@proton.me>
2025-09-11 10:22:13 +0200

    Base basique d'event pipi de base

---
+++ My project/Assets/Scripts/ScrEvent.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PipiEvents", order = 2, fileName = "New pissing event")]
+public class ScrEvent : ScriptableObject
+{
+    public string eventName = "Pipi Event";
+}
